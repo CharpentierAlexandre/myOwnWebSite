aaa26f1b3a64bda703ccd22fddfbfb8a93175701
Alexandre Charpentier <alexandre.charpentier.ac@gmail.com>
2017-06-03 09:29:08 +0200

    my first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>MyOwnWebSite</title>
+    
+    </head>
+    <body>
+        <h1>This is my own Website</h1>
+        <p>
+        Here is my first Hello World!
+        </p>
+    </body>
+</html>
